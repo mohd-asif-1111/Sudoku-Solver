7a235e2a487ab3435581eb58a70caca92cb0add9
Mohd Asif <mr422577@gmail.com>
2026-07-06 19:02:07 +0530

    Optimized Using MRV heuristic

--- sudoku solver.cpp
+++ sudoku solver.cpp
@@ -2,11 +2,21 @@
 #include <utility>
 #include <vector>
 
-void printBoard(const std::vector<std::vector<int>> &sudoku)
+using Cell = std::pair<int, int>;
+using Board = std::vector<std::vector<int>>;
+
+constexpr Cell None{-1, -1};
+
+int calls = 0;
+int backtracks = 0;
+
+bool debug = true;
+
+void printBoard(const Board &sudoku)
 {
-	for (const std::vector<int>& row : sudoku)
+	for (const std::vector<int> &row : sudoku)
 	{
-		for (const int& col : row)
+		for (const int &col : row)
 		{
 			std::cout << col << ' ';
 		}
@@ -14,36 +24,18 @@ void printBoard(const std::vector<std::vector<int>> &sudoku)
 	}
 }
 
-std::pair<int, int> findEmpty(const std::vector<std::vector<int>> &sudoku)
-{
-	std::pair<int, int> cell;
-	cell = {-1, -1};
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (sudoku[i][j] == 0)
-			{
-				cell = {i, j};
-				return cell;
-			}
-		}
-	}
-	return cell;
-}
-
-bool valid(const std::vector<std::vector<int>> &sudoku, std::pair<int, int> cell, int num)
+bool valid(const Board &sudoku, const Cell &cell, int num)
 {
 	int r = cell.first;
 	int c = cell.second;
-	for (const int& row : sudoku[r])
+	for (const int row : sudoku[r])
 	{
 		if (row == num)
 		{
 			return false;
 		}
 	}
-	for (const std::vector<int>& row : sudoku)
+	for (const std::vector<int> &row : sudoku)
 	{
 		if (row[c] == num)
 		{
@@ -65,10 +57,61 @@ bool valid(const std::vector<std::vector<int>> &sudoku, std::pair<int, int> cell
 	return true;
 }
 
-bool solve(std::vector<std::vector<int>> &sudoku)
+std::vector<int> candidates(const Board &sudoku, const Cell &cell)
 {
-	std::pair<int, int> empty_cell = findEmpty(sudoku);
-	std::pair<int, int> None = {-1, -1};
+	std::vector<int> cands;
+	for (int num = 1; num <= 9; num++)
+	{
+		if (valid(sudoku, cell, num))
+		{
+			cands.push_back(num);
+		}
+	}
+	return cands;
+}
+
+Cell bestEmptyCell(const Board &sudoku)
+{
+	Cell bestCell;
+	std::vector<int> bestCands;
+	std::vector<int> cands;
+
+	bestCell = None;
+	bestCands = {0};
+
+	for (int r = 0; r < 9; r++)
+	{
+		for (int c = 0; c < 9; c++)
+		{
+			if (sudoku[r][c] == 0)
+			{
+				cands = candidates(sudoku, {r, c});
+				if (cands.size() == 1)
+				{
+					bestCell = {r, c};
+					return bestCell;
+				}
+
+				if (bestCell == None)
+				{
+					bestCell = {r, c};
+					bestCands = cands;
+				}
+				else if (cands.size() < bestCands.size())
+				{
+					bestCell = {r, c};
+					bestCands = cands;
+				}
+			}
+		}
+	}
+	return bestCell;
+}
+
+bool solve(Board &sudoku)
+{
+	calls++;
+	Cell empty_cell = bestEmptyCell(sudoku);
 	if (empty_cell == None)
 	{
 		return true;
@@ -76,28 +119,34 @@ bool solve(std::vector<std::vector<int>> &sudoku)
 	int r = empty_cell.first;
 	int c = empty_cell.second;
 
-	for (int num = 1; num <= 9; num++)
+	std::vector<int> cands;
+	cands = candidates(sudoku, empty_cell);
+
+   if(cands.empty()){
+      return false;
+   }
+
+	for (int &cand : cands)
 	{
-		if (valid(sudoku, empty_cell, num))
+		sudoku[r][c] = cand;
+
+		if (solve(sudoku))
 		{
-			sudoku[r][c] = num;
-			
-			if (solve(sudoku))
-			{
-				return true;
-			}
-			
-			sudoku[r][c] = 0;
+			return true;
 		}
+
+		sudoku[r][c] = 0;
+
+		backtracks++;
 	}
 	return false;
 }
 
 int main()
 {
-	std::vector<std::vector<int>> sudoku;
+	Board sudoku;
 	sudoku = {
-		{1, 2, 2, 0, 9, 0, 0, 0, 5},
+		{1, 2, 0, 0, 9, 0, 0, 0, 5},
 		{0, 6, 0, 0, 0, 0, 0, 2, 0},
 		{0, 4, 0, 7, 2, 0, 0, 0, 0},
 		{0, 1, 0, 0, 0, 0, 0, 7, 0},
@@ -105,8 +154,7 @@ int main()
 		{0, 7, 0, 0, 0, 0, 0, 0, 8},
 		{2, 0, 0, 0, 5, 0, 0, 0, 3},
 		{0, 8, 0, 4, 0, 0, 0, 9, 0},
-		{0, 5, 0, 0, 0, 3, 0, 0, 1}
-	};
+		{0, 5, 0, 0, 0, 3, 0, 0, 1}};
 
 	if (solve(sudoku))
 	{
@@ -118,4 +166,11 @@ int main()
 	}
 
 	printBoard(sudoku);
+
+	if (debug)
+	{
+		std::cout << "Calls: " << calls;
+		std::cout << '\n';
+		std::cout << "Backtracks: " << backtracks;
+	}
 }
